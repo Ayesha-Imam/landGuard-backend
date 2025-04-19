c5a832ffde7e62404e88105fd120552455a9bb49
Ayesha-Imam <ayesha.imam786786@gmail.com>
2025-04-20 01:28:43 +0500

    Fixed CORS error

--- landguard_be/landguard_be/settings.py
+++ landguard_be/landguard_be/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'django_extensions'
+    'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,15 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'landguard_be.urls'
+
+# for development only
+# CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",   
+    "http://127.0.0.1:3000",   
+]
+
 
 TEMPLATES = [
     {
